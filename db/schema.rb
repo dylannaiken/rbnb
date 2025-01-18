39636cfeaf5c7591605f9502c672339118b27a27
Dorian Desfour <dorian.desfour.pro@gmail.com>
2025-01-18 14:34:30 +0100

    model booking created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_18_132657) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_18_133340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_132657) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "startDate"
+    t.datetime "endDate"
+    t.bigint "appartment_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["appartment_id"], name: "index_bookings_on_appartment_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +46,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_132657) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "appartments"
+  add_foreign_key "bookings", "users"
 end

f8624d2c19f2a5a3652b081ca208caa816feb790
memphis mootoosamy <memphismootoosamy@gmail.com>
2025-01-23 19:03:40 +0400

    controller-review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,50 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[ show edit update destroy ]
+
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  #def show
+  #end
+
+  def new
+    @review = Review.new
+  end
+
+  #def edit 
+  #end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review, notice: "Review was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+#   def update
+#     if @review.update(review_params)
+#       redirect_to @review, notice: "Review was successfully updated.", status: :see_other
+#     else
+#       render :edit, status: :unprocessable_entity
+#     end
+#   end
+
+#   def destroy
+#     @review.destroy!
+#     redirect_to reviews_url, notice: "Review was successfully destroyed.", status: :see_other
+#   end
+
+   private
+
+#     def set_review
+#       @review = Review.find(params[:id])
+#     end
+
+     def review_params
+       params.require(:review).permit(:name, :address, :opening_date)
+     end
+ end

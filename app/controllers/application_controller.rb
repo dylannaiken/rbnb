6fae579c775074d4392dc2632fd7dbf0793aebf2
dilkoo14 <dilshad_nohur@hotmail.fr>
2025-02-01 13:25:13 +0100

    feature-show-html1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address)
+  end
 end
